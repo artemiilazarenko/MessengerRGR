9170a952cfe1f16354bb14e04fca22ecb3a2c274
artem <artemke2002@mail.ru>
2022-05-17 21:16:10 +0300

    index page

--- src/main/java/rgr/Messenger/Entity/User.java
+++ src/main/java/rgr/Messenger/Entity/User.java
@@ -49,6 +49,10 @@ public class User implements UserDetails {
         return username;
     }
 
+    public Long getId() {
+        return id;
+    }
+
     public String getEmail() {
         return email;
     }
