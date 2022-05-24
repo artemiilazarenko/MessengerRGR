3712c4eaa9fa413837519186d640312226e231e6
kidh1337 <kidh.mlggaming@gmail.com>
2022-05-24 16:44:01 +0400

    dialog ui

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -2,10 +2,16 @@ function sendMessage(form) {
     const XHR = new XMLHttpRequest();
     const FD = new FormData(form);
     XHR.addEventListener("load", function(event) {
+        message_input.value = ''
+        console.log(XHR.responseText)
     } );
     XHR.addEventListener("error", function( event) {
         console.log('Oops! Something went wrong.');
     } );
     XHR.open("POST", form.action);
     XHR.send(FD);
+}
+
+function updateMessages(id) {
+    setTimeout(updateMessages, 300);
 }
