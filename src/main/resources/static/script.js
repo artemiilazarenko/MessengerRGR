35cf060f3eac8ff259ff08b444bf6c94c24b4596
kidh1337 <kidh.mlggaming@gmail.com>
2022-05-25 19:07:09 +0400

    real time message updating

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -12,6 +12,43 @@ function sendMessage(form) {
     XHR.send(FD);
 }
 
-function updateMessages(id) {
-    setTimeout(updateMessages, 300);
+function updateMessages() {
+    setTimeout(updateMessages, 500);
+    getMessages(dialog.getAttribute('value'))
+}
+
+window.onload = updateMessages
+
+function getMessages(id) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "/dialogs/getMessages/" + id);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onload = () => parseMessages(xhr.responseText);
+    xhr.send();
+}
+
+function parseMessages(messages) {
+    let msgs = JSON.parse(messages)
+    var arr = Object.keys(msgs).map(key => [key, msgs[key]]).sort((a, b) => b[0]-a[0])
+    messages_div.innerHTML = ''
+    for(let el in arr) {
+        let msg = createMessageDiv(arr[el][1]['author']['id'], arr[el][1]['author']['firstName'], arr[el][1]['date'], arr[el][1]['text'])
+        messages_div.appendChild(msg)
+    }
+}
+
+function createMessageDiv(author_id, author_firstName, date, text) {
+    let msdiv = document.createElement('div')
+    msdiv.classList.add('message')
+    msdiv.innerHTML = `<div class="message_inf_block">
+                         <a href="/profile/${author_id}">${author_firstName}</a>
+                         <div class="message_inf_date">
+                             ${date}
+                         </div>
+                     </div>
+                     <div class="message_text_block">
+                         ${text}
+                     </div>`
+    return msdiv
 }
